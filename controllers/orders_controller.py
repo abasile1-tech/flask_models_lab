376a89ebb069c9e25c941b317710c4013935de95
John <ashton.basile@gmail.com>
2023-07-18 15:19:32 +0100

    I completed the first extension by adding links to the individual order pages

--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -6,3 +6,8 @@ orders_blueprint = Blueprint("orders", __name__)
 @orders_blueprint.route("/orders")
 def index():
 	return render_template("index.html", title="Order List", order_list = orders)
+
+@orders_blueprint.route("/orders/<id>")
+def order(id):
+	id = int(id)
+	return render_template("order.html",title="Order", order=orders[id])
