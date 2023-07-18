2afd256b0c7ca899b280f5820721d2dd72461ecd
John <ashton.basile@gmail.com>
2023-07-18 16:00:33 +0100

    I added a description that only shows up on the individual customer page

--- models/order.py
+++ models/order.py
@@ -1,7 +1,8 @@
 class Order:
-	def __init__(self, id, customer_name, order_date, quantity, item):
+	def __init__(self, id, customer_name, order_date, quantity, item, description):
 		self.id = id
 		self.customer_name = customer_name
 		self.order_date =order_date
 		self.quantity = quantity
 		self.item = item
+		self.description = description
--- models/orders.py
+++ models/orders.py
@@ -1,7 +1,7 @@
 from .order import Order
 
-john_pizza_order = Order(0, "John", "27 Feb 2020", 2, "Pizza")
-john_drink_order = Order(1, "John", "27 Feb 2020", 2, "Drink")
-sally_calzone_order = Order(2, "Sally", "31 Dec 2023", 4, "Calzone")
+john_pizza_order = Order(0, "John", "27 Feb 2020", 2, "Pizza", "The customer wants the pepperoni pizzas to be gluten-free and lactose-free")
+john_drink_order = Order(1, "John", "27 Feb 2020", 2, "Drink", "The customer wants one Dr. Pepper and one Root Beer.")
+sally_calzone_order = Order(2, "Sally", "31 Dec 2023", 4, "Calzone", "The customer is allergic to pine nuts, so the pesto needs to be made fresh (without pine nuts)")
 
 orders = [john_pizza_order, john_drink_order, sally_calzone_order]
